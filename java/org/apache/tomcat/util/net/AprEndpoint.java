apache/tomcat80

commit b3a68256f7758ce8fe52306fa0388f7084d7a5ba

Ensure sendfile is enabled by default for APR.
git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1530269 13f79535-47bb-0310-9956-ffa450edef68

java/org/apache/tomcat/util/net/AprEndpoint.java
@@ -150,8 +150,19 @@ public class AprEndpoint extends AbstractEndpoint<Long> {
     /**
      * Use sendfile for sending static files.
      */
-    protected boolean useSendfile = Library.APR_HAS_SENDFILE;
-    public void setUseSendfile(boolean useSendfile) { this.useSendfile = useSendfile; }
+    protected boolean useSendfile = false;
+    /*
+     * When the endpoint is created and configured, the APR library will not
+     * have been initialised. This flag is used to determine if the default
+     * value of useSendFile should be changed if the APR library indicates it
+     * supports send file once it has been initialised. If useSendFile is set
+     * by configuration, that configuration will always take priority.
+     */
+    private boolean useSendFileSet = false;
+    public void setUseSendfile(boolean useSendfile) {
+        useSendFileSet = true;
+        this.useSendfile = useSendfile;
+    }
     @Override
     public boolean getUseSendfile() { return useSendfile; }
 
@@ -606,6 +617,9 @@ public class AprEndpoint extends AbstractEndpoint<Long> {
             pollerThread.start();
 
             // Start sendfile thread
+            if (!useSendFileSet) {
+                useSendfile = Library.APR_HAS_SENDFILE;
+            }
             if (useSendfile) {
                 sendfile = new Sendfile();
                 sendfile.init();

commit 4f22607265302c555e06c61073820668f9f31b43

Fix the regression in r1530269. Enabling/disabling sendfile should occur during bind not during start. Add a warning message if the user tries to explicitly enable sendfile with SSL.
git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1531659 13f79535-47bb-0310-9956-ffa450edef68

java/org/apache/tomcat/util/net/AprEndpoint.java
@@ -457,8 +457,11 @@ public class AprEndpoint extends AbstractEndpoint<Long> {
             Socket.optSet(serverSock, Socket.APR_SO_REUSEADDR, 1);
         }
 
-        // Sendfile usage on systems which don't support it cause major problems
-        if (useSendfile && !Library.APR_HAS_SENDFILE) {
+        // Enable Sendfile by default if it has not been configured but usage on
+        // systems which don't support it cause major problems
+        if (!useSendFileSet) {
+            useSendfile = Library.APR_HAS_SENDFILE;
+        } else if (useSendfile && !Library.APR_HAS_SENDFILE) {
             useSendfile = false;
         }
 
@@ -583,7 +586,12 @@ public class AprEndpoint extends AbstractEndpoint<Long> {
             }
             SSLContext.setVerify(sslContext, value, SSLVerifyDepth);
             // For now, sendfile is not supported with SSL
-            useSendfile = false;
+            if (useSendfile) {
+                useSendfile = false;
+                if (useSendFileSet) {
+                    log.warn(sm.getString("endpoint.apr.noSendfileWithSSL"));
+                }
+            }
         }
     }
 
@@ -617,9 +625,6 @@ public class AprEndpoint extends AbstractEndpoint<Long> {
             pollerThread.start();
 
             // Start sendfile thread
-            if (!useSendFileSet) {
-                useSendfile = Library.APR_HAS_SENDFILE;
-            }
             if (useSendfile) {
                 sendfile = new Sendfile();
                 sendfile.init();

java/org/apache/tomcat/util/net/res/LocalStrings.properties
@@ -55,4 +55,5 @@ endpoint.apr.pollAddInvalid=Invalid attempted to add a socket [{0}] to the polle
 endpoint.apr.pollError=Poller failed with error [{0}] : [{1}]
 endpoint.apr.pollUnknownEvent=A socket was returned from the poller with an unrecognized event [{0}]
 endpoint.apr.invalidSslProtocol=An invalid value [{0}] was provided for the SSLProtocol attribute
+endpoint.apr.noSendfileWithSSL=Sendfile is not supported for the APR/native connector when SSL is enabled
 endpoint.nio.selectorCloseFail=Failed to close selector when closing the poller