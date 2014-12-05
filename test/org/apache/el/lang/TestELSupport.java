c8ad438fcb1324d4a6994d16569de1c0c6240de2
Mark Thomas <markt@apache.org>
2014-12-05 17:39:06 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=57309 Ensure that the current EL Resolver is given an opportunity to perform type coercion before applying the default EL coercion rules.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc8.0.x/trunk@1643367 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/el/lang/TestELSupport.java
+++ test/org/apache/el/lang/TestELSupport.java
@@ -33,7 +33,7 @@ import org.junit.Test;
 public class TestELSupport {
     @Test
     public void testEquals() {
-        assertTrue(ELSupport.equals("01", Long.valueOf(1)));
+        assertTrue(ELSupport.equals(null, "01", Long.valueOf(1)));
     }
 
     @Test
@@ -81,13 +81,13 @@ public class TestELSupport {
     @Test
     public void testCoerceIntegerToNumber() {
         Integer input = Integer.valueOf(4390241);
-        Object output = ELSupport.coerceToType(input, Number.class);
+        Object output = ELSupport.coerceToType(null, input, Number.class);
         assertEquals(input, output);
     }
 
     @Test
     public void testCoerceNullToNumber() {
-        Object output = ELSupport.coerceToType(null, Number.class);
+        Object output = ELSupport.coerceToType(null, null, Number.class);
         assertNull(output);
     }
 
@@ -95,7 +95,7 @@ public class TestELSupport {
     public void testCoerceEnumAToEnumA() {
         Object output = null;
         try {
-            output = ELSupport.coerceToEnum(TestEnumA.VALA1, TestEnumA.class);
+            output = ELSupport.coerceToEnum(null, TestEnumA.VALA1, TestEnumA.class);
         } finally {
             assertEquals(TestEnumA.VALA1, output);
         }
@@ -105,7 +105,7 @@ public class TestELSupport {
     public void testCoerceEnumAToEnumB() {
         Object output = null;
         try {
-            output = ELSupport.coerceToEnum(TestEnumA.VALA1, TestEnumB.class);
+            output = ELSupport.coerceToEnum(null, TestEnumA.VALA1, TestEnumB.class);
         } catch (ELException ele) {
             // Ignore
         }
@@ -116,7 +116,7 @@ public class TestELSupport {
     public void testCoerceEnumAToEnumC() {
         Object output = null;
         try {
-            output = ELSupport.coerceToEnum(TestEnumA.VALA1, TestEnumC.class);
+            output = ELSupport.coerceToEnum(null, TestEnumA.VALA1, TestEnumC.class);
         } catch (ELException ele) {
             // Ignore
         }
@@ -243,31 +243,31 @@ public class TestELSupport {
     @Test
     public void testCoerceToNumber01() {
         Object result = ELSupport.coerceToNumber(
-                (Object) null, Integer.class);
+                null, (Object) null, Integer.class);
         Assert.assertNull("Resut: " + result, result);
     }
 
     @Test
     public void testCoerceToNumber02() {
         Object result = ELSupport.coerceToNumber(
-                (Object) null, int.class);
+                null, (Object) null, int.class);
         Assert.assertEquals(Integer.valueOf(0), result);
     }
 
     @Test
     public void testCoerceToBoolean01() {
-        Object result = ELSupport.coerceToBoolean(null, true);
+        Object result = ELSupport.coerceToBoolean(null, null, true);
         Assert.assertEquals(Boolean.FALSE, result);
     }
 
     @Test
     public void testCoerceToBoolean02() {
-        Object result = ELSupport.coerceToBoolean(null, false);
+        Object result = ELSupport.coerceToBoolean(null, null, false);
         Assert.assertNull("Resut: " + result, result);
     }
 
     private static void testIsSame(Object value) {
-        assertEquals(value, ELSupport.coerceToNumber(value, value.getClass()));
+        assertEquals(value, ELSupport.coerceToNumber(null, value, value.getClass()));
     }
 
     private static enum TestEnumA {
