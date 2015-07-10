apache/tomcat80

commit be1399bc0c160d6d888ce4dc661f3d18fcbe463f

Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=52725 Use constant rather than hard-coded package name since package name is meant to be configurable.
git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1297213 13f79535-47bb-0310-9956-ffa450edef68

java/org/apache/jasper/compiler/JspUtil.java
@@ -26,6 +26,7 @@ import java.util.Vector;
 import java.util.jar.JarFile;
 import java.util.zip.ZipEntry;
 
+import org.apache.jasper.Constants;
 import org.apache.jasper.JasperException;
 import org.apache.jasper.JspCompilationContext;
 import org.xml.sax.Attributes;
@@ -738,7 +739,7 @@ public class JspUtil {
 
         index = path.indexOf(WEB_INF_TAGS);
         if (index != -1) {
-            className = "org.apache.jsp.tag.web.";
+            className = Constants.TAG_FILE_PACKAGE_NAME + ".web";
             begin = index + WEB_INF_TAGS.length();
         } else {
             index = path.indexOf(META_INF_TAGS);
@@ -756,7 +757,8 @@ public class JspUtil {
     }
 
     private static String getClassNameBase(String urn) {
-        StringBuilder base = new StringBuilder("org.apache.jsp.tag.meta.");
+        StringBuilder base =
+                new StringBuilder(Constants.TAG_FILE_PACKAGE_NAME + ".meta.");
         if (urn != null) {
             base.append(makeJavaPackage(urn));
             base.append('.');

commit 1a8c25a86a05b950b33837a2aeb162b5ccd1e6cf

Fix https://bz.apache.org/bugzilla/show_bug.cgi?id=58119 Merged revision 1690209 from tomcat/trunk: When tags are compiled they must be placed in the org\apache\jsp\tag\web directory. Fixed a regression made with r1297213.
git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc8.0.x/trunk@1690213 13f79535-47bb-0310-9956-ffa450edef68

java/org/apache/jasper/compiler/JspUtil.java
@@ -716,7 +716,7 @@ public class JspUtil {
 
         index = path.indexOf(WEB_INF_TAGS);
         if (index != -1) {
-            className = Constants.TAG_FILE_PACKAGE_NAME + ".web";
+            className = Constants.TAG_FILE_PACKAGE_NAME + ".web.";
             begin = index + WEB_INF_TAGS.length();
         } else {
             index = path.indexOf(META_INF_TAGS);