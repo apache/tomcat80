aaa9ec4dc95fc5755e0ef205290bacb4f15a38ea
Mark Thomas <markt@apache.org>
2015-12-18 11:27:01 +0000

    Fix https://bz.apache.org/bugzilla/show_bug.cgi?id=58702 Ensure an access log entry is generated if the client aborts the connection.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc8.0.x/trunk@1720756 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/connector/CoyoteAdapter.java
+++ java/org/apache/catalina/connector/CoyoteAdapter.java
@@ -418,15 +418,6 @@ public class CoyoteAdapter implements Adapter {
             if (!request.isAsync() && !comet) {
                 request.finishRequest();
                 response.finishResponse();
-                if (request.getMappingData().context != null) {
-                    request.getMappingData().context.logAccess(
-                            request, response,
-                            System.currentTimeMillis() - req.getStartTime(),
-                            false);
-                } else {
-                    // Should normally not happen
-                    log(req, res, System.currentTimeMillis() - req.getStartTime());
-                }
             }
 
             // Check to see if the processor is in an error state. If it is,
@@ -446,12 +437,21 @@ public class CoyoteAdapter implements Adapter {
         } finally {
             if (!success) {
                 res.setStatus(500);
+            }
+
+            // Access logging
+            if (!success || !request.isAsync() && !comet) {
                 long time = 0;
                 if (req.getStartTime() != -1) {
                     time = System.currentTimeMillis() - req.getStartTime();
                 }
-                log(req, res, time);
+                if (request.getMappingData().context != null) {
+                    request.getMappingData().context.logAccess(request, response, time, false);
+                } else {
+                    log(req, res, time);
+                }
             }
+
             req.getRequestProcessor().setWorkerThreadName(null);
             // Recycle the wrapper request and response
             if (!success || (!comet && !request.isAsync())) {
@@ -466,6 +466,7 @@ public class CoyoteAdapter implements Adapter {
         }
         return success;
     }
+
 
     /**
      * Service method.
@@ -507,13 +508,13 @@ public class CoyoteAdapter implements Adapter {
 
         boolean comet = false;
         boolean async = false;
+        boolean postParseSuccess = false;
 
         try {
-
             // Parse and set Catalina and configuration specific
             // request parameters
             req.getRequestProcessor().setWorkerThreadName(THREAD_NAME.get());
-            boolean postParseSuccess = postParseRequest(req, request, res, response);
+            postParseSuccess = postParseRequest(req, request, res, response);
             if (postParseSuccess) {
                 //check valves if we support async
                 request.setAsyncSupported(connector.getService().getContainer().getPipeline().isAsyncSupported());
@@ -556,8 +557,13 @@ public class CoyoteAdapter implements Adapter {
             } else if (!comet) {
                 request.finishRequest();
                 response.finishResponse();
-                if (postParseSuccess &&
-                        request.getMappingData().context != null) {
+            }
+        } catch (IOException e) {
+            // Ignore
+        } finally {
+            // Access log
+            if (!async && !comet) {
+                if (postParseSuccess) {
                     // Log only if processing was invoked.
                     // If postParseRequest() failed, it has already logged it.
                     // If context is null this was the start of a comet request
@@ -569,12 +575,10 @@ public class CoyoteAdapter implements Adapter {
                 }
             }
 
-        } catch (IOException e) {
-            // Ignore
-        } finally {
             req.getRequestProcessor().setWorkerThreadName(null);
             AtomicBoolean error = new AtomicBoolean(false);
             res.action(ActionCode.IS_ERROR, error);
+
             // Recycle the wrapper request and response
             if (!comet && !async || error.get()) {
                 request.recycle();
