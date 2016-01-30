580f54716b003efe1efe355fe15eeb73ca778647
Mark Thomas <markt@apache.org>
2016-01-30 00:49:57 +0000

    Fix https://bz.apache.org/bugzilla/show_bug.cgi?id=58768 Log a warning if a redirect fails because of an invalid location.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc8.0.x/trunk@1727676 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/connector/Response.java
+++ java/org/apache/catalina/connector/Response.java
@@ -51,6 +51,8 @@ import org.apache.catalina.util.RequestUtil;
 import org.apache.catalina.util.SessionConfig;
 import org.apache.catalina.util.UriUtil;
 import org.apache.coyote.ActionCode;
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
 import org.apache.tomcat.util.buf.CharChunk;
 import org.apache.tomcat.util.buf.UEncoder;
 import org.apache.tomcat.util.buf.UEncoder.SafeCharsSet;
@@ -65,14 +67,12 @@ import org.apache.tomcat.util.res.StringManager;
  * @author Remy Maucherat
  * @author Craig R. McClanahan
  */
-public class Response
-    implements HttpServletResponse {
+public class Response implements HttpServletResponse {
 
+    private static final Log log = LogFactory.getLog(Response.class);
+    protected static final StringManager sm = StringManager.getManager(Response.class);
 
-    // ----------------------------------------------------------- Constructors
-
-    private static final MediaTypeCache MEDIA_TYPE_CACHE =
-            new MediaTypeCache(100);
+    private static final MediaTypeCache MEDIA_TYPE_CACHE = new MediaTypeCache(100);
 
     /**
      * Compliance with SRV.15.2.22.1. A call to Response.getWriter() if no
@@ -87,18 +87,6 @@ public class Response
                 System.getProperty("org.apache.catalina.connector.Response.ENFORCE_ENCODING_IN_GET_WRITER",
                         "true"));
     }
-
-    public Response() {
-    }
-
-
-    // ----------------------------------------------------- Class Variables
-
-    /**
-     * The string manager for this package.
-     */
-    protected static final StringManager sm =
-        StringManager.getManager(Constants.Package);
 
 
     // ----------------------------------------------------- Instance Variables
@@ -1302,6 +1290,7 @@ public class Response
                 flushBuffer();
             }
         } catch (IllegalArgumentException e) {
+            log.warn(sm.getString("response.sendRedirectFail", location), e);
             setStatus(SC_NOT_FOUND);
         }
 
