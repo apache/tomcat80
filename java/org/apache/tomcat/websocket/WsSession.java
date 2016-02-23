c6aade416683fceac489d49e2a76ce2ff041fc3d
Mark Thomas <markt@apache.org>
2016-02-23 14:41:54 +0000

    Fix https://bz.apache.org/bugzilla/show_bug.cgi?id=59014 Fix problem that meant close messages could not be sent because the session was already marked as closed.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc8.0.x/trunk@1731867 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/websocket/WsSession.java
+++ java/org/apache/tomcat/websocket/WsSession.java
@@ -651,7 +651,8 @@ public class WsSession implements Session {
             // If the session has already been closed the any registered futures
             // will have been processed so the failure result for this future
             // needs to be set here.
-            if (isOpen()) {
+            if (isOpen() || f2sh.isCloseMessage()) {
+                // WebSocket session is open or this is the close message
                 futures.put(f2sh, f2sh);
             } else if (f2sh.isDone()) {
                 // NO-OP. The future completed before the session closed so no
