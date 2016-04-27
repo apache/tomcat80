ed09bbabe1011836e570b84fe5a802b728bbc3bf
Mark Thomas <markt@apache.org>
2016-04-27 09:23:57 +0000

    Review of Map / ConcurrentMap / ConcurrentHashMap Declare field using interface rather than implementation.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc8.0.x/trunk@1741176 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/core/StandardContext.java
+++ java/org/apache/catalina/core/StandardContext.java
@@ -42,6 +42,7 @@ import java.util.Set;
 import java.util.Stack;
 import java.util.TreeMap;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.locks.Lock;
@@ -461,7 +462,7 @@ public class StandardContext extends ContainerBase
      * The context initialization parameters for this web application,
      * keyed by name.
      */
-    private final ConcurrentHashMap<String, String> parameters = new ConcurrentHashMap<>();
+    private final ConcurrentMap<String, String> parameters = new ConcurrentHashMap<>();
 
 
     /**
