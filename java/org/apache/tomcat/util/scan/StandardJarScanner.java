34dc6e62e78b61dfbd836dc2afa688104bf97c7b
Violeta Georgieva Georgieva <violetagg@apache.org>
2016-07-11 21:08:12 +0000

    Fix https://bz.apache.org/bugzilla/show_bug.cgi?id=59813 Ensure that circular relations of the Class-Path attribute from JAR manifests will be processed correctly.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc8.0.x/trunk@1752217 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/scan/StandardJarScanner.java
+++ java/org/apache/tomcat/util/scan/StandardJarScanner.java
@@ -269,6 +269,7 @@ public class StandardJarScanner implements JarScanner {
                                 log.debug(sm.getString("jarScan.classloaderJarScan", url));
                             }
                             try {
+                                processedURLs.add(url);
                                 process(scanType, callback, url, null, isWebapp, classPathUrlsToProcess);
                             } catch (IOException ioe) {
                                 log.warn(sm.getString("jarScan.classloaderFail", url), ioe);
