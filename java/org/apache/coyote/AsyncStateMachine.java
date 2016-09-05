apache/tomcat80

commit adc68190b42de6bee5c17c1dee13acf6821852db

Always clear the nonBlocking listeners on complete. If complete is called from a non-container thread a number of states are possible. It is more robust to always clear the listeners.
git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc8.0.x/trunk@1759229 13f79535-47bb-0310-9956-ffa450edef68

java/org/apache/coyote/AsyncStateMachine.java
@@ -260,6 +260,7 @@ public class AsyncStateMachine {
 
     public synchronized boolean asyncComplete() {
         pauseNonContainerThread();
+        clearNonBlockingListeners();
         boolean doComplete = false;
         if (state == AsyncState.STARTING) {
             state = AsyncState.MUST_COMPLETE;
@@ -270,7 +271,6 @@ public class AsyncStateMachine {
                 state == AsyncState.ERROR) {
             state = AsyncState.MUST_COMPLETE;
         } else if (state == AsyncState.READ_WRITE_OP) {
-            clearNonBlockingListeners();
             state = AsyncState.MUST_COMPLETE;
         } else {
             throw new IllegalStateException(

commit ce087d21ce4a29e6520783fa38049a3f8064f9ae

Correct regression in r1759229
git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc8.0.x/trunk@1759255 13f79535-47bb-0310-9956-ffa450edef68

java/org/apache/coyote/AsyncStateMachine.java
@@ -236,7 +236,10 @@ public class AsyncStateMachine {
             asyncCtxt.fireOnComplete();
             state = AsyncState.DISPATCHED;
             return SocketState.ASYNC_END;
-        } else if (state == AsyncState.MUST_DISPATCH || state == AsyncState.DISPATCHING) {
+        } else if (state == AsyncState.MUST_DISPATCH) {
+            state = AsyncState.DISPATCHING;
+            return SocketState.ASYNC_END;
+        } else if (state == AsyncState.DISPATCHING) {
             state = AsyncState.DISPATCHED;
             return SocketState.ASYNC_END;
         } else if (state == AsyncState.STARTED) {