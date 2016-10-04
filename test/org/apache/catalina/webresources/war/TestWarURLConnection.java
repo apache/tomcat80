b2d7fa0a07c4cf30d79435dd4f390fac51c717e6
Mark Thomas <markt@apache.org>
2016-10-04 11:37:26 +0000

    Move the WAR handling code to a separate package to provide the option to register it via the java.protocol.handler.pkgs system property
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc8.0.x/trunk@1763266 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/catalina/webresources/war/TestWarURLConnection.java
+++ test/org/apache/catalina/webresources/war/TestWarURLConnection.java
@@ -14,17 +14,39 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.catalina.webresources;
+package org.apache.catalina.webresources.war;
 
-import java.io.IOException;
+import java.io.File;
 import java.net.URL;
 import java.net.URLConnection;
-import java.net.URLStreamHandler;
 
-public class WarURLStreamHandler extends URLStreamHandler {
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
-    @Override
-    protected URLConnection openConnection(URL u) throws IOException {
-        return new WarURLConnection(u);
+import org.apache.catalina.webresources.TomcatURLStreamHandlerFactory;
+
+public class TestWarURLConnection {
+
+    @Before
+    public void register() {
+        TomcatURLStreamHandlerFactory.register();
+    }
+
+
+    @Test
+    public void testContentLength() throws Exception {
+        File f = new File("test/webresources/war-url-connection.war");
+        String fileUrl = f.toURI().toURL().toString();
+
+        URL indexHtmlUrl = new URL("jar:war:" + fileUrl +
+                "*/WEB-INF/lib/test.jar!/META-INF/resources/index.html");
+
+        URLConnection urlConn = indexHtmlUrl.openConnection();
+        urlConn.connect();
+
+        int size = urlConn.getContentLength();
+
+        Assert.assertEquals(137, size);
     }
 }
