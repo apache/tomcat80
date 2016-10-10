184703b966fab28a27eddf4a49014bf92e5a34eb
Mark Thomas <markt@apache.org>
2016-10-10 13:08:15 +0000

    Exclude JAR files in /WEB-INF/lib from the static resource cache.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc8.0.x/trunk@1764085 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/webresources/Cache.java
+++ java/org/apache/catalina/webresources/Cache.java
@@ -200,9 +200,12 @@ public class Cache {
     }
 
     private boolean noCache(String path) {
-        // Don't cache resources used by the class loader (it has its own cache)
-        if (path.startsWith("/WEB-INF/classes/") ||
-                path.startsWith("/WEB-INF/lib/")) {
+        // Don't cache classes. The class loader handles this.
+        // Don't cache JARs. The ResourceSet handles this.
+        if ((path.endsWith(".class") &&
+                (path.startsWith("/WEB-INF/classes/") || path.startsWith("/WEB-INF/lib/")))
+                ||
+                (path.startsWith("/WEB-INF/lib/") && path.endsWith(".jar"))) {
             return true;
         }
         return false;
