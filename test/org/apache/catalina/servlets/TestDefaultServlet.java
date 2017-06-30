7771313e640fc0da80f269f27065ef20057ee455
Mark Thomas <markt@apache.org>
2017-06-30 21:29:54 +0000

    Fix CI failures
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc8.0.x/trunk@1800470 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/catalina/servlets/TestDefaultServlet.java
+++ test/org/apache/catalina/servlets/TestDefaultServlet.java
@@ -472,7 +472,7 @@ public class TestDefaultServlet extends TomcatBaseTest {
         } else {
             res.setCharset(StandardCharsets.ISO_8859_1);
         }
-        Assert.assertEquals("½", res.toString());
+        Assert.assertEquals("\u00bd", res.toString());
     }
 
     @Test
@@ -508,7 +508,7 @@ public class TestDefaultServlet extends TomcatBaseTest {
         } else {
             res.setCharset(StandardCharsets.ISO_8859_1);
         }
-        Assert.assertEquals("½", res.toString());
+        Assert.assertEquals("\u00bd", res.toString());
     }
 
     private static class EncodingServlet extends HttpServlet {
