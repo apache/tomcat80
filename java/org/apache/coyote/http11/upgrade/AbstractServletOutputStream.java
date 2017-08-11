3e82e28a8eab2136cde8018994e4e4e8429954a1
Remy Maucherat <remm@apache.org>
2017-08-11 16:10:32 +0000

    Fix possible race condition when using an upgraded connection and setting the IO listeners, it now uses the same processing as non upgraded connections.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc8.0.x/trunk@1804815 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/http11/upgrade/AbstractServletOutputStream.java
+++ java/org/apache/coyote/http11/upgrade/AbstractServletOutputStream.java
@@ -100,14 +100,14 @@ public abstract class AbstractServletOutputStream<S> extends ServletOutputStream
             throw new IllegalArgumentException(
                     sm.getString("upgrade.sos.writeListener.set"));
         }
+        this.listener = listener;
+        this.applicationLoader = Thread.currentThread().getContextClassLoader();
         // Container is responsible for first call to onWritePossible() but only
         // need to do this if setting the listener for the first time.
         synchronized (fireListenerLock) {
             fireListener = true;
         }
         socketWrapper.addDispatch(DispatchType.NON_BLOCKING_WRITE);
-        this.listener = listener;
-        this.applicationLoader = Thread.currentThread().getContextClassLoader();
     }
 
 
