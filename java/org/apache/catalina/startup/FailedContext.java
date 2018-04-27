d3ffb5c7fbff89ac29f737bce6330b823148afa1
Mark Thomas <markt@apache.org>
2018-04-27 17:16:28 +0000

    Collapse multiple leading '/' characters to a single '/' in the return value of HttpServletRequest#getContextPath() to avoid issues if the value is used with HttpServletResponse#sendRedirect(). This behaviour is enabled by default and configurable via the new Context attribute allowMultipleLeadingForwardSlashInPath.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc8.0.x/trunk@1830369 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/startup/FailedContext.java
+++ java/org/apache/catalina/startup/FailedContext.java
@@ -803,4 +803,12 @@ public class FailedContext extends LifecycleMBeanBase implements Context {
     public void setDispatchersUseEncodedPaths(boolean dispatchersUseEncodedPaths) { /* NO-OP */ }
     @Override
     public boolean getDispatchersUseEncodedPaths() { return true; }
+
+    @Override
+    public void setAllowMultipleLeadingForwardSlashInPath(
+            boolean allowMultipleLeadingForwardSlashInPath) {
+        // NO-OP
+    }
+    @Override
+    public boolean getAllowMultipleLeadingForwardSlashInPath() { return false; }
 }
